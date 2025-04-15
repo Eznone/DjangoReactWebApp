28916a0a99a16a92657a9764d3f1cbc45828e8a7
Enzo <enzotresmediano@gmail.com>
2025-04-15 10:24:58 -0300

    Created POST for students API

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('studentsView/', views.studentsView),
+    path('students/', views.students),
 ]
--- api/views.py
+++ api/views.py
@@ -7,11 +7,21 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 
 # Without api_view we dont get the cool UI
-@api_view(['GET'])
-def studentsView(request):
+@api_view(['GET', 'POST'])
+def students(request):
     if request.method == 'GET':
         # Get all data from Student table
         students = Student.objects.all()
         # Many makes it so we fetch various students
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # Else not necessary but good visualization
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

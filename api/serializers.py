e032b69ee4cfcf0268dbb6d5b1f609eec1ce5889
Enzo <enzotresmediano@gmail.com>
2025-04-24 08:47:40 -0300

    Added EmployeeSerializer to serializaers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,18 @@
 # requires rest_framework in django_rest_main
 from rest_framework import serializers
 from students.models import Student
+from employees.models import Employee
 
 class StudentSerializer(serializers.ModelSerializer):
-    
     # Must be called Meta so that framework finds the class
     class Meta:
         model = Student
         fields = "__all__"
         # fields = ['student_id', 'name', 'branch']
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    # Must be called Meta so that framework finds the class
+    class Meta:
+        model = Employee
+        fields = "__all__"
+        # fields = ['emp_id', 'emp_name', 'designation']

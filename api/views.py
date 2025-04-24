dad86409aa50ffadf14ab36d5f6355373ad22a63
Enzo <enzotresmediano@gmail.com>
2025-04-24 09:05:32 -0300

    Inital get and post of class based view

--- api/views.py
+++ api/views.py
@@ -1,10 +1,16 @@
 # from django.shortcuts import render
 # from django.http import JsonResponse
+# imports for models
 from students.models import Student
-from .serializers import StudentSerializer
+from employees.models import Employee
+# Necessary imports for basic API functionality
+from .serializers import StudentSerializer, EmployeeSerializer
 from rest_framework.response import Response
 from rest_framework import status
+# Decorator function
 from rest_framework.decorators import api_view
+# Class based view
+from rest_framework.views import APIView
 
 # Without api_view we dont get the cool UI
 @api_view(['GET', 'POST'])
@@ -49,3 +55,18 @@ def studentDetailView(request, pk):
     elif request.method == 'DELETE':
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class Employees(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
